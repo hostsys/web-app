f0d37fe232c4f6c96a9cb6de85f5f72261ccb614
hostsys <udelg322@gmail.com>
2024-07-09 19:31:40 -0400

    Start of new routing/component system

--- uriah/router/router.js
+++ uriah/router/router.js
@@ -1,115 +1,57 @@
-document.addEventListener("click", (e) => {
-  const { target } = e;
-  if (!target.matches("a")) {
-    return;
-  }
-  e.preventDefault();
-  urlRoute();
-});
-
-const urlRoutes = {
-  404: {
-    template: "/src/pages/404.html",
-    title: "404",
-    description: "",
-  },
-  "/": {
-    template: "/src/pages/home.html",
-    title: "/home",
-    description: "",
-  },
-  "/portfolio": {
-    template: "/src/pages/portfolio.html",
-    title: "/portfolio",
-    description: "",
-  },
-  "/controls": {
-    template: "/src/pages/controls.html",
-    title: "/controls",
-    description: "",
-  },
-  "/music": {
-    template: "/src/pages/music.html",
-    title: "/music",
-    description: "",
-  },
-  "/gallery": {
-    template: "/src/pages/gallery.html",
-    title: "/gallery",
-    description: "",
-  },
-  "/the-box": {
-    template: "/src/pages/the-box.html",
-    title: "/the-box",
-    description: "",
-  },
-};
-
-const navEvent = new CustomEvent("navEvent");
-
-const urlRoute = (event) => {
-  event = event || window.event;
-  event.preventDefault();
-  window.history.pushState({}, "", event.target.href);
-  window.dispatchEvent(navEvent);
-  urlLocationHandler();
-};
-
-const loadedScripts = new Set();
-const urlLocationHandler = async () => {
-  let location = window.location.pathname;
-  if (location.length == 0) {
-    location = "/";
+class Router {
+  constructor(routes) {
+    this.routes = routes;
+    this.init();
   }
 
-  const route = urlRoutes[location] || urlRoutes[404];
-  const html = await fetch(route.template).then((response) => response.text());
+  init() {
+    this.handleRoute(window.location.pathname);
+    window.onpopstate = () => this.handleRoute(window.location.pathname);
 
-  document.getElementById("view").innerHTML = html;
-  document.getElementById("page-header").innerHTML = route.title;
+    document.addEventListener("click", (e) => {
+      const { target } = e;
+      if (!target.matches("a")) {
+        return;
+      }
+      e.preventDefault();
+      history.pushState({}, "", e.target.href);
+      this.handleRoute(window.location.pathname);
+    });
+  }
 
-  const existingScripts = document.querySelectorAll("script[dynamic-src]");
-  existingScripts.forEach((script) => {
-    script.parentNode.removeChild(script);
-  });
+  handleRoute(route) {
+    if (route.length === 0) route = "/";
+    if (this.routes[route]) {
+      this.loadComponent(route);
+    } else {
+      this.loadComponent("404");
+    }
+  }
 
-  const scriptTags = new DOMParser()
-    .parseFromString(html, "text/html")
-    .querySelectorAll("script");
+  async loadComponent(route) {
+    window.dispatchEvent(new CustomEvent("navEvent"));
+    const view = document.getElementById("view");
+    view.innerHTML = "";
 
-  scriptTags.forEach((scriptTag) => {
-    // console.log(scriptTag);
-    const scriptContent = scriptTag.innerHTML;
-
-    // const scriptPath = scriptTag.getAttribute("dynamic-src");
-
-    // console.log("script path: ", scriptPath);
-    // console.log("scripts before creation: ", loadedScripts);
-    if (scriptTag.getAttribute("src") !== "/@vite/client") {
-      // console.log("loaded script for this path not found, creating!");
-      const script = document.createElement("script");
-      script.textContent = scriptContent;
-      script.setAttribute("type", "module");
-      script.setAttribute("dynamic-src", location);
-      document.head.appendChild(script);
-      // loadedScripts.add(location);
-      // console.log("scripts after creation: ", loadedScripts);
-      // console.log(scriptTags);
-    } // else {
-    // console.log("loaded script for this path found, NOT creating new one");
-    // }
-  });
-};
-
-if (import.meta.hot) {
-  import.meta.hot.accept(async (newModule) => {
-    // This code will run when Vite detects changes and triggers a hot update
-
-    // Fetch the updated HTML content and update the DOM
-    await urlLocationHandler();
-  });
+    const { default: Component } = await this.routes[route].component();
+    const component = document.createElement(Component.tagName);
+    view.appendChild(component);
+  }
 }
 
-window.onpopstate = urlLocationHandler();
-window.route = urlRoute;
-// urlLocationHandler();
+const routes = {
+  "/": {
+    component: () => import("../src/js/HomeComponent"),
+    title: "Home",
+  },
+  "/two": {
+    component: () => import("../src/js/pageTwoComponent"),
+    title: "Page Two",
+  },
+  404: {
+    component: () => import("../src/js/404"),
+    title: "Page Two",
+  },
+};
+
+const router = new Router(routes);
